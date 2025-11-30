c51c8944e891348417f9a081952c002db2d925c8
Xolo978 <prithvirajbanik900@gmail.com>
2025-11-30 14:12:08 +0530

    Added recurse insertion and selection

--- Week8/sort/insertion.c
+++ Week8/sort/insertion.c
@@ -4,17 +4,18 @@ void insertionSort(int arr[], int n) {
   int i, j, key;
   for (int i = 1; i < n; i++) {
     // Chose the element
-    int curr = arr[i];
+    int key = arr[i];
     // Then pick the starting point of comparison which is before the curr
     // element's index From this index onwards we shift the elements towards
     // right till we find the correct place for the curr element
-    int prev = i - 1;
+    // From j and before we have the sorted array
+    int j = i - 1;
     // Go over as long as we dont hit the first element and beyond and as long
-    // as the prev elements are grater than the curr element
-
-    while (j >= 0 && arr[j] > curr) {
-      // Shift the elements towards right
+    // as the prev elements are greater than the curr element
+    while (j >= 0 && arr[j] > key) {
+      // Shift the elements towards right and make space for the curr element
       arr[j + 1] = arr[j];
+      // Keep looking for its correct place
       j = j - 1;
     }
     // Found the position for the curr elements
@@ -27,6 +28,25 @@ void printArray(int arr[], int n) {
     printf("%d ", arr[i]);
   }
   printf("\n");
+}
+
+void recursiveSort(int arr[], int n) {
+  if (n <= 1)
+    return;
+  // First recursively sort n-1 elements
+  recursiveSort(arr, n - 1);
+  // Get the last item from the sorted array(which is not yet sorted probably
+  // and is our target to sort or the current element)
+  int key = arr[n - 1];
+  // This is the last element from the sorted part
+  int j = n - 2;
+  // Keep shifting till we get the position
+  // https://www.youtube.com/watch?v=A8DPwEk8Has
+  while (j >= 0 && arr[j] > key) {
+    arr[j + 1] = arr[j];
+    j = j - 1;
+  }
+  arr[j + 1] = key;
 }
 
 int main() {
--- Week8/sort/selection.c
+++ Week8/sort/selection.c
@@ -1,13 +1,64 @@
+#include <stdio.h>
+/**Selection sort is nothing but dividing the array into sorted and unsorted
+ part Initially there is only unsorted part We pick smallest element from the
+ unsorted part and then swap with the first element in the unsorted part This
+ increases the sorted part and decrease the unsorted part
+ **/
 void sort(int *arr, int n) {
+  // By the time we reach last element the last two elements would already be
+  // sorted since the second last element must be <= last according to the algo
   for (int i = 0; i < n - 1; i++) {
+    // Take smallest as the current element(i to n-1 is unsorted) and then find
+    // the smallest element from then onwards
     int smallest = i;
     for (int j = i + 1; j < n; j++) {
       if (arr[j] < arr[smallest]) {
         smallest = j;
       }
     }
+    // Now swap the smallest element with the 1st element in the unsorted part
+    // which is ith index
     int temp = arr[i];
     arr[i] = arr[smallest];
     arr[smallest] = temp;
   }
+}
+
+void recurseSort(int arr[], int n, int start) {
+  // Start is the start of the unsorted array
+  int minId = start;
+  for (int i = start; i < n; i++) {
+    if (arr[i] < arr[minId]) {
+      minId = i;
+    }
+  }
+  // Now that we have the minElement from the unsorted part we can now swap with
+  // the start element
+  int temp = arr[start];
+  arr[start] = arr[minId];
+  arr[minId] = temp;
+  // Do this thing for the rest of the array
+  recurseSort(arr, n, start + 1);
+}
+
+void printArray(int arr[], int n) {
+  for (int i = 0; i < n; ++i) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
+
+int main() {
+  int arr[] = {12, 11, 13, 5, 6, 7};
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  printf("Original array: ");
+  printArray(arr, n);
+
+  sort(arr, n);
+
+  printf("Sorted array: ");
+  printArray(arr, n);
+
+  return 0;
 }
